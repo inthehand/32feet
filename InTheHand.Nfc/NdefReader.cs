cca131161609568e887ffcd814bb25ca9e4b369b
Peter Foot <peter@inthehand.com>
2020-08-01 18:05:48 +0100

    Skeleton NFC

---
+++ InTheHand.Nfc/NdefReader.cs
@@ -0,0 +1,25 @@
+﻿// 32feet.NET - Personal Area Networking for .NET
+//
+// InTheHand.Nfc.NdefReader
+// 
+// Copyright (c) 2020 In The Hand Ltd, All rights reserved.
+// This source code is licensed under the MIT License
+
+using System;
+using System.Threading.Tasks;
+
+namespace InTheHand.Nfc
+{
+    public sealed class NdefReader
+    {
+
+        public Task ScanAsync(NdefScanOptions options = null)
+        {
+            return Task.FromException(new NotSupportedException());
+        }
+
+        public event EventHandler Reading;
+
+        public event EventHandler Error;
+    }
+}
---
+++ InTheHand.Nfc/NdefScanOptions.cs
@@ -0,0 +1,22 @@
+﻿// 32feet.NET - Personal Area Networking for .NET
+//
+// InTheHand.Nfc.NdefScanOptions
+// 
+// Copyright (c) 2020 In The Hand Ltd, All rights reserved.
+// This source code is licensed under the MIT License
+
+using System.Threading;
+
+namespace InTheHand.Nfc
+{
+    public class NdefScanOptions
+    {
+        public string Id { get; set; }
+
+        public string RecordType { get; set; }
+
+        public string MediaType { get; set; }
+
+        public CancellationToken Signal { get; set; }     
+    }
+}
