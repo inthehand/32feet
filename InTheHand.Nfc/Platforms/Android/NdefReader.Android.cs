1d886a094479641be594c8ab8528e7fd613a0f0c
Peter Foot <peter@inthehand.com>
2020-09-08 21:34:39 +0100

    Basics of Android implementation.

--- InTheHand.Nfc/Platforms/Android/NdefReader.Android.cs
+++ InTheHand.Nfc/Platforms/Android/NdefReader.Android.cs
@@ -8,19 +8,42 @@
 using System;
 using System.Threading.Tasks;
 using Android.App;
+using Android.Content;
 using Android.Nfc;
+using Android.Nfc.Tech;
 
 [assembly: UsesPermission("android.permission.NFC")]
+[assembly: UsesFeature("android.hardware.nfc", Required = false)]
 
 namespace InTheHand.Nfc
 {
-    partial class NdefReader
+    partial class NdefReader : Java.Lang.Object, NfcAdapter.IReaderCallback
     {
         static readonly NfcAdapter s_adapter = NfcAdapter.GetDefaultAdapter(Application.Context);
 
-        private Task PlatformScanAsync(NdefScanOptions options = null)
+        void NfcAdapter.IReaderCallback.OnTagDiscovered(Tag tag)
         {
-            return Task.FromException(new PlatformNotSupportedException());
+            var techList = tag.GetTechList();
+            foreach(string tech in techList)
+            {
+                if(tech == "android.nfc.tech.Ndef")
+                {
+                    var ndef = Ndef.Get(tag);
+                    ndef.Connect();
+                    var msg = ndef.NdefMessage;
+                    ndef.Dispose();
+                    
+                    // parse message
+
+                    Reading?.Invoke(this, null);
+                    break;
+                }
+            }
+        }
+
+        private async Task PlatformScanAsync(NdefScanOptions options = null)
+        {
+            s_adapter.EnableReaderMode(Xamarin.Essentials.Platform.CurrentActivity, this, NfcReaderFlags.NfcA | NfcReaderFlags.NfcB | NfcReaderFlags.NfcF | NfcReaderFlags.NfcV, null);
         }
     }
 }
