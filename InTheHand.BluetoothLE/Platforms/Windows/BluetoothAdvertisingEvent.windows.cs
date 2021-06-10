a91704076ebecc00aba976d094f41d82eda82edc
TravisRo <libusbdotnet@gmail.com>
2021-06-10 16:10:32 -0600

    Update BluetoothAdvertisingEvent.windows.cs
    Remove unused using directive

--- InTheHand.BluetoothLE/Platforms/Windows/BluetoothAdvertisingEvent.windows.cs
+++ InTheHand.BluetoothLE/Platforms/Windows/BluetoothAdvertisingEvent.windows.cs
@@ -8,7 +8,6 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
